9281a27ab2ce2f523a1aa2ffe1189afedc8e628e
ganza12 <tuyisengear@gmail.com>
2021-09-06 03:22:04 +0200

    stagging next message

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        byakunze se musaza
+        <img src={logo} className="App-logo" alt="logo" />       
       </header>
+      <p> byakunze se musaza</p>
     </div>
   );
 }
